f565560048e5c0b36abfc5dd3bf25acd2b8b63db
Felipe Valenzuela <fvalenzuela-ctr@semantic-ai.com>
2025-03-20 16:15:47 -0700

    Se actualizo una prueba.

--- cypress/e2e/editarActividad.cy.ts
+++ cypress/e2e/editarActividad.cy.ts
@@ -5,6 +5,13 @@ describe('Editar una actividad', () => {
     // Visitar la página principal
     cy.visit('/')
     cy.validacionInicial();
-    
+
+     // Agregar una actividad
+     cy.get(e2e.CAMPO_AGREGAR_ACTIVIDAD).click();
+     cy.get(e2e.CAMPO_AGREGAR_ACTIVIDAD).type('Actividad de prueba');
+ 
+     cy.get(e2e.BOTON_AGREGAR).click();
+ 
+     cy.get(e2e.TEXT_AREA).should('have.text', 'Actividad de prueba');
   })
 })
